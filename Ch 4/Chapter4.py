bc9e329b1094e4cb8a198c132f51fb39cb86eeee
maurice60 <maurice.williams@me.com>
2014-11-16 17:05:28 +0000

    Week 4 genomePatternWalk and overlapGraph day end

--- Ch 4/Chapter4.py
+++ Ch 4/Chapter4.py
@@ -1,8 +1,8 @@
 __author__ = 'maurice'
 import sys
 import os
-import math
-import random
+# import math
+# import random
 
 sys.path.append(os.path.relpath('../'))
 from common import *
@@ -20,11 +20,29 @@ def overlapGraph(dna):
             out.append((s, h))
     return sorted(out)
 
-# out = composition(fRead('dna.txt'), 100, sort=True)
+# out = composition('0000111100101101000', 4, sort=True)
 # out = genomePathWalk(aReadT('dna.txt'))
-out = overlapGraph(aReadT('dna.txt'))
+# out = overlapGraph(aReadT('dna.txt'))
 # print out
-with open('outf.txt', 'w') as fil:
-    for x in out:
-        # print '{0} -> {1}'.format(*x)
-        fil.writelines('{0} -> {1}\n'.format(*x))
+# with open('outf.txt', 'w') as fil:
+#     for x in out:
+#         # print '{0} -> {1}'.format(*x)
+#         fil.writelines('{0} -> {1}\n'.format(*x))
+# a = 14
+# l = []
+# while True:
+#     l.append(a)
+#     if len(l) == 2**4:
+#         break
+#     a = (2*a) % (2**4)
+#     while a in l:
+#         a = (2*a + 1) % (2**4)
+#
+# bstr = ['{0:04b}'.format(d) for d in l]
+#
+# print genomePathWalk(bstr)
+# print l, bstr
+og = [(a,b) for (a,b) in overlapGraph(['{0:04b}'.format(d) for d in range(2**4)]) if a != b]
+
+
+
