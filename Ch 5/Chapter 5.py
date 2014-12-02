a5c51e3efdeb8be65108c2693f363ebb70a10eb6
maurice60 <maurice.williams@me.com>
2014-12-02 17:19:08 +0000

    Chapter 5 paths, coins

--- Ch 5/Chapter 5.py
+++ Ch 5/Chapter 5.py
@@ -1,1 +1,21 @@
 __author__ = 'maurice'
+from math import factorial
+
+def paths(m, n):
+    return factorial(m+n)/(factorial(m)*factorial(n))
+
+def coins(denom, n):
+    m = n + 1
+    ans = {i:0 for i in range(m)}
+    vals = [i for i in denom if i < m]
+    z = 1
+    while vals != []:
+        for i in vals:
+            ans[i] = z
+        new = set(j+c for j in vals for c in denom)
+        vals = [p for p in new if p < m and ans[p] == 0]
+        z += 1
+    return ans
+
+
+print coins([9,5,3,1],  19163)
